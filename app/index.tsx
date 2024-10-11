cc1c0904bd5c83ae5c85191cf88f9c7ba80e2140
ihsanrm99 <ihsanrahmatm@gmail.com>
2024-10-12 00:23:01 +0700

    Navigate between screens

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,14 @@
-// The app/index.tsx file defines the text displayed on the app's screen. It is the entry point of our app and executes when the development server starts.
-
-import {
-  /* @tutinfo Import <CODE>StyleSheet</CODE> to define styles. */ StyleSheet,
-  Text,
-  View,
-} from "react-native";
+/* @tutinfo Import <CODE>Link</CODE> component from <CODE>expo-router</CODE>. */ import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      {/* @tutinfo This used to say "Edit app/index.tsx to edit this screen". Now it says, "Home screen". */}
       <Text style={styles.text}>Home screen</Text>
+      {/* @tutinfo Add <CODE>Link</CODE> component after <CODE>Text</CODE> component and pass the <CODE>href</CODE> prop with <CODE>/about</CODE> route. */}
+      <Link href="/about" style={styles.button}>
+        Go to About screen
+      </Link>
     </View>
   );
 }
@@ -18,12 +16,17 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    /* @tutinfo Add the value of <CODE>backgroundColor</CODE> property with <CODE>'#25292e'</CODE>.*/
     backgroundColor: "#25292e",
     alignItems: "center",
     justifyContent: "center",
   },
   text: {
+    color: "#fff",
+  },
+  /* @tutinfo Add the style of <CODE>fontSize</CODE>, <CODE>textDecorationLine</CODE>, and <CODE>color</CODE> to <CODE>Link</CODE> component. */
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
     color: "#fff",
   },
 });
